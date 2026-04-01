fcc80ccda207200aacd821476114b1dfcfa98eb2
KimJuYeong <43029921+dafher801@users.noreply.github.com>
2026-04-02 03:19:37 +0900

    Task 1-3-1,2,3 완료 (이슈)
    Task 1-3-1, 1-3-2가 이미 이전 Task에 의해 완료되어 있었으며, 작업 범위가 서로 침범하여 충돌한 것으로 보인다.

---
+++ Assets/Scripts/Entity/LegalActionSet.cs
@@ -0,0 +1,23 @@
+﻿// LegalActionSet.cs
+// ActionValidator가 반환하는 합법 액션 정보를 담는 구조체.
+// 특정 플레이어가 현재 취할 수 있는 액션 종류와 금액 범위를 포함한다.
+
+using System.Collections.Generic;
+
+namespace TexasHoldem.Entity
+{
+    public struct LegalActionSet
+    {
+        // 수행 가능한 액션 종류 목록
+        public List<ActionType> AvailableActions;
+
+        // 콜에 필요한 금액. 콜 불가 시 0
+        public int CallAmount;
+
+        // 최소 레이즈 총액 (자신의 CurrentBet 기준 총 베팅 금액)
+        public int MinRaiseAmount;
+
+        // 최대 레이즈 총액 (올인 금액)
+        public int MaxRaiseAmount;
+    }
+}
